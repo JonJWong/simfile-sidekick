3c7924de8c8231f81bc6e488e36114215bd640fc
JonJWong <wongjonathan01@gmail.com>
2023-10-24 03:30:53 -0400

    Add box logic

--- bot.py
+++ bot.py
@@ -392,8 +392,9 @@ def create_embed(data, ctx):
     if "-xtras" in ctx.message.content.split(" "):
         pattern_analysis = __append_pattern_info("Double stairs", "double_stairs", data, pattern_analysis)
         pattern_analysis = __append_pattern_info("Doublesteps", "doublesteps", data, pattern_analysis)
-        pattern_analysis = __append_pattern_info("Jumps", "jumps", data, pattern_analysis)
+        pattern_analysis = __append_pattern_info("Mid Stream Jumps", "jumps", data, pattern_analysis)
         pattern_analysis = __append_pattern_info("Monos", "mono", data, pattern_analysis)
+        pattern_analysis = __append_pattern_info("Boxs", "box", data, pattern_analysis)
 
     embed.add_field(name="__Pattern Analysis__", value=pattern_analysis,
                     inline=False)
